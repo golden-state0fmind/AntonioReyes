9c57b9178f9363f2b10ace0b84938e3725ecb8be
golden-state0fmind <areyes2009@yahoo.com>
2021-01-06 21:06:41 -0700

    cleans up unused code

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon')
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
-
-// dark or light images
-// function imageMode(color) {
-//     image1.src = `img/undraw_proud_coder_${color}.svg`;
-//     image2.src = `img/undraw_feeling_proud_${color}.svg`;
-//     image3.src = `img/undraw_conceptual_idea_${color}.svg`;
-// };
 
 //lightDarkToggle
 function toggleDarkLightMode(isDark) {
@@ -20,7 +10,6 @@ function toggleDarkLightMode(isDark) {
     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
     isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') :
         toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    // isDark ? imageMode('dark') : imageMode('light')
 };
 
 // switch theme dynamically
@@ -35,7 +24,7 @@ function switchTheme(event) {
         toggleDarkLightMode(false);
     }
 }
-//event listeneer
+//event listener
 toggleSwitch.addEventListener('change', switchTheme);
 
 // check local storage
